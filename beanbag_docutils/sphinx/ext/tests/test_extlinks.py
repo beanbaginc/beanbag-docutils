aaf6cd52a987a45685da2f2228a4db7645ac5bf6
Christian Hammond <christian@beanbaginc.com>
2024-07-22 16:42:30 -0700

    Add support for Sphinx 7.4.
    Sphinx 7.4 (released July 15, 2024) added a new CSS class for links
    generated using the extlink extension. This didn't impact usage of
    beanbag-docutils, but did regress a unit test.
    
    Short of any uncaught failures, we can now claim support for Sphinx 7.4.
    
    Testing Done:
    Unit tests pass with Sphinx < 7.x and latest 7.x.
    
    Reviewed at https://reviews.reviewboard.org/r/14049/

--- beanbag_docutils/sphinx/ext/tests/test_extlinks.py
+++ beanbag_docutils/sphinx/ext/tests/test_extlinks.py
@@ -26,7 +26,7 @@ class ExtLinksTests(SphinxExtTestCase):
 
     def test_extlink_with_anchor(self):
         """Testing :<extlink>:`...` role with mapped URL containing anchor"""
-        self.assertEqual(
+        self.assertRegex(
             self.render_doc(':bug:`123`'),
-            '<p><a class="reference external"'
-            ' href="https://bugs.example.com/123?#main">bug123</a></p>')
+            r'<p><a class="(extlink-bug )?reference external"'
+            r' href="https://bugs.example.com/123\?#main">bug123</a></p>')
